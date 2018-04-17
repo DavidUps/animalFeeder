a8b71cf8ac1438c1c3ef5b42805339732b733cba
david <davidarribas.ciclos@gmail.com>
2018-04-18 00:56:42 +0200

    Fragments

--- app/src/main/java/com/example/cfgs/animalfeeder/activities/MainActivity.java
+++ app/src/main/java/com/example/cfgs/animalfeeder/activities/MainActivity.java
@@ -3,65 +3,21 @@ package com.example.cfgs.animalfeeder.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.brouding.blockbutton.BlockButton;
 import com.example.cfgs.animalfeeder.R;
+import com.example.cfgs.animalfeeder.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    BlockButton btnProfile, btnSettings, btnBowl, btnList, btnChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnProfile  = findViewById(R.id.btnProfile);
-        btnSettings = findViewById(R.id.btnSettings);
-        btnBowl     = findViewById(R.id.btnBowl);
-        btnList     = findViewById(R.id.btnList);
-        btnChat     = findViewById(R.id.btnChat);
+        getSupportFragmentManager().beginTransaction().replace(R.id.containerFragment, new MainFragment()).addToBackStack("Main").commit();
 
-        btnProfile.setEnabled(true);
-        btnSettings.setEnabled(true);
-        btnBowl.setEnabled(true);
-        btnList.setEnabled(true);
-        btnChat.setEnabled(true);
-
-        btnProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-
-        btnSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        btnBowl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        btnList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        btnChat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/example/cfgs/animalfeeder/fragments/MainFragment.java
@@ -0,0 +1,92 @@
+package com.example.cfgs.animalfeeder.fragments;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.brouding.blockbutton.BlockButton;
+import com.example.cfgs.animalfeeder.R;
+
+public class MainFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    BlockButton btnProfile, btnSettings, btnBowl, btnList, btnChat;
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        btnProfile  = view.findViewById(R.id.btnProfile);
+        btnSettings = view.findViewById(R.id.btnSettings);
+        btnBowl     = view.findViewById(R.id.btnBowl);
+        btnList     = view.findViewById(R.id.btnList);
+        btnChat     = view.findViewById(R.id.btnChat);
+
+        btnProfile.setEnabled(true);
+        btnSettings.setEnabled(true);
+        btnList.setEnabled(true);
+        btnChat.setEnabled(true);
+        btnBowl.setEnabled(true);
+
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profileFragment();
+            }
+        });
+
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        btnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        btnBowl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        return view;
+    }
+
+    public void profileFragment() {
+        
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
